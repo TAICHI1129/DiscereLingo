4c14467ff4f2199db3acf6b314dea7b0491e940e
TAICHI1129 <train.x956@gmail.com>
2026-01-31 21:05:03 +0900

    Add LANGUAGES array for Jeneleseez language

---
+++ langdata.js
@@ -0,0 +1,11 @@
+const LANGUAGES = [
+  {
+    id: "jeneleseez",
+    name: "Jeneleseez",
+    author: "Taichi",
+    words: [
+      { word: "Waz", meaning: "なぜ", pos: "疑問詞" },
+      { word: "go", meaning: "行く", pos: "動詞" }
+    ]
+  }
+];
